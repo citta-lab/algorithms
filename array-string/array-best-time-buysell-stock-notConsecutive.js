5f548d2e56113164c66b6b2d22018a9777b72589
Mahesh Sampekatte <mahesh.sampekatte@cerner.com>
2019-01-25 00:11:27 -0600

    stock

---
+++ array-string/array-best-time-buysell-stock-notConsecutive.js
@@ -0,0 +1,18 @@
+/**
+ * Question: Maximum profit buy selling the stock
+ * Input: [7,1,5,3,6,4]
+    Output: 5
+    Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+    Not 7-1 = 6, as selling price needs to be larger than buying price.
+ */
+
+var maxProfit = function(prices) {
+    var min = Number.MAX_SAFE_INTEGER; 
+    var max = 0;
+    for (var i = 0; i < prices.length; i++) {
+        min = Math.min(min, prices[i]);
+        max = Math.max(max, prices[i] - min);
+    }
+    return max;
+};
+
